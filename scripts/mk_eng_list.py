aee898f5d1dd16b035db3f40e279fbefa60fcc8f
John Bent <john.bent@seagate.com>
2022-07-05 10:07:20 -0600

    Updated for python3

--- scripts/mk_eng_list.py
+++ scripts/mk_eng_list.py
@@ -10,7 +10,7 @@ import belau
 def collect(db,c,query,field,table):
   distinct = set()
 
-  print "Execute %s" % query
+  print("Execute %s" % query)
   c.execute(query)
   for row in c.fetchall():
     clean = re.sub('e.g.',' ', row[field])
@@ -19,7 +19,7 @@ def collect(db,c,query,field,table):
     words = clean.split(' ') 
     distinct.update(words)
 
-  print "Fetched the %s words" % field
+  print("Fetched the %s words" % field)
 
   # somehow a blank word is added 
   distinct.remove('')
@@ -28,7 +28,7 @@ def collect(db,c,query,field,table):
   c.execute("""drop table if exists `%s`""" % table);
   c.execute("""create table `%s` ( `%s` varchar(32) NOT NULL DEFAULT '' )""" % (table,field));
   q = "insert into `%s` (`%s`) values %s" % (table,field,values)
-  print q
+  print(q)
   c.execute(q)
 
 def main():
